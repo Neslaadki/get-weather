74d551dd767edb6c4ed6fddc777e137ff49e45fa
Dzyubak Anton <toha5420&@gmail.com>
2020-02-15 00:05:58 +0300

    beta 1.02

--- weather3.py
+++ weather3.py
@@ -1,23 +1,22 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
+import pyown
 
 
+def weather(place):
+    if place == "Exit":
+        exit(0)
+    else:
+        owm = pyowm.OWM('a25b6ba061d2fea55447080b88118f3f', language='ru')
 
-import pyowm
+        place = input(' Привет, в каком городе хочешь узнать погоду? ')  # укажи город без пробела
 
-owm = pyowm.OWM('a25b6ba061d2fea55447080b88118f3f',  language = 'ru')  
+        observation = owm.weather_at_place(place)
+        w = observation.get_weather()
 
+        temperature = w.get_temperature('celsius')['temp']
 
-place = input(' Привет, в каком городе хочешь узнать погоду? ') # укажи город без пробела
-
-
-observation = owm.weather_at_place(place)
-w = observation.get_weather()
-
-temperature = w.get_temperature('celsius')['temp']
+        print(" В городе " + place + " сейчас " + str(temperature) + " по цельсию ")
+        print(" В этом городе " + w.get_detailed_status() + ", оденься нормально, плез!  ")
+        weather(place)
 
 
-
-
-print(" В городе " + place + " сейчас " + str(temperature) + " по цельсию " )                     
-print(" В этом городе " + w.get_detailed_status() + ", оденься нормально, плез!  ")
+weather(place)
